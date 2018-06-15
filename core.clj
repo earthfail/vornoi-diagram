54827482f49600c1024f7a5471796a3d7f199e08
earthfail <saleemkhatib22@gmail.com>
2018-06-15 18:02:31 +0300

    squash! clean up

--- core.clj
+++ core.clj
@@ -12,23 +12,38 @@
    (sort-by val)
    ffirst))
 
-
+(def uri "http://www.catbreedselector.com/wp-content/uploads/2015/09/LaPerm-Cat-Images.jpg")
 (defn setup []
-  (let [img (q/resize (q/load-image "cat2.jpg") (q/width) (q/height))]
-    
-    {:img img}
-    )
-  )
+  (let [img (q/request-image uri)]
+    {:img img
+     :dots (into {}
+                 (for [x (range 5
+                                (- (q/width) 5) 50)
+                       y (range 5
+                                (- (q/height) 5) 50)
+                       :let [c (q/get-pixel img x y 50 50)]]
+                   [[x y] c]))}))
 
-(defn draw [state]
-  (if (:img state)
-    (q/image (:img state) 0 0))
-  )
+(defn draw [{img :img dots :dots}]
+  ;; (q/stroke-weight 5)
+  ;; (doseq [[p c] dots]
+  ;;   (apply q/stroke c)
+  ;;   (apply q/point p))
+  ;; (q/stroke-weight 1)
+  ;; (doseq [rp (repeatedly 100 rand-point)]
+  ;;   (let [closestp (closest-point rp (keys dots))
+  ;;         color (dots closestp)]
+  ;;     (apply q/stroke color)
+  ;;     (apply q/point rp)))
+  (if img
+    (q/image img 0 0)))
 
 (q/defsketch trigonometry
   :size size
   :draw draw
   :setup setup
   :update identity
+  :features [:no-safe-fns :resizable]
   :mouse-pressed (fn [state event] (println state) state)
+  :key-pressed (fn [state event] (println state) state)
   :middleware [m/fun-mode m/pause-on-error])
