a1d25c80773272752f28457a155c21259c3596c7
earthfail <saleemkhatib22@gmail.com>
2018-06-16 01:28:20 +0300

    make a working demo for vornoi-diagram on pictures

--- core.clj
+++ core.clj
@@ -1,7 +1,6 @@
 (ns my-sketch.core
   (:require [quil.core :as q]
             [quil.middleware :as m]))
-(def size [500 500])
 (defn rand-point []
   (mapv (partial rand-int) [(q/width) (q/height)]))
 (defn closest-point [[x y] statesp]
@@ -11,39 +10,54 @@
    (zipmap statesp)
    (sort-by val)
    ffirst))
-
-(def uri "http://www.catbreedselector.com/wp-content/uploads/2015/09/LaPerm-Cat-Images.jpg")
+(defn rand-col []
+  (into {}
+        (for [point (repeatedly 200 rand-point)
+              :let [[x y] point
+                    c (q/get-pixel x y)]]
+          [point c])))
+(def uri "/home/surrlim/Pictures/glasses_pencil_shorthair.png")
 (defn setup []
-  (let [img (q/request-image uri)]
+  (q/no-loop)
+  (let [img (q/load-image uri)]
+    (q/image img 0 0)
     {:img img
-     :dots (into {}
-                 (for [x (range 5
-                                (- (q/width) 5) 50)
-                       y (range 5
-                                (- (q/height) 5) 50)
-                       :let [c (q/get-pixel img x y 50 50)]]
-                   [[x y] c]))}))
+     :dots (rand-col)}))
 
 (defn draw [{img :img dots :dots}]
-  ;; (q/stroke-weight 5)
-  ;; (doseq [[p c] dots]
-  ;;   (apply q/stroke c)
-  ;;   (apply q/point p))
-  ;; (q/stroke-weight 1)
-  ;; (doseq [rp (repeatedly 100 rand-point)]
-  ;;   (let [closestp (closest-point rp (keys dots))
-  ;;         color (dots closestp)]
-  ;;     (apply q/stroke color)
-  ;;     (apply q/point rp)))
-  (if img
-    (q/image img 0 0)))
+  ;; (println "redraw!")
+  (q/stroke-weight 5)
+  (doseq [[p c] dots]
+    (q/stroke c)
+    (apply q/point p))
+  (q/stroke-weight 1)
+  (doseq [rp (repeatedly 100 rand-point)]
+    (let [closestp (closest-point rp (keys dots))
+          color (dots closestp)]
+      (q/stroke-weight 10)
+      (q/stroke color)
+      (apply q/point rp)))
+  (q/save-frame "resource/####.png")
+  )
 
 (q/defsketch trigonometry
-  :size size
+  :size [1000 1000]
   :draw draw
   :setup setup
   :update identity
-  :features [:no-safe-fns :resizable]
-  :mouse-pressed (fn [state event] (println state) state)
-  :key-pressed (fn [state event] (println state) state)
-  :middleware [m/fun-mode m/pause-on-error])
+  :features [:resizable]
+  :mouse-pressed (fn [state {:keys [x y]}] (println x y) state)
+  :key-pressed (fn [state {k :key}]
+                 (if-let [new-dots
+                          (case k
+                            :a (q/no-loop)
+                            :s (q/start-loop)
+                            :d (q/redraw)
+                            :c (q/clear)
+                            :b (q/image (:img state) 0 0)
+                            :r (rand-col)
+                            :w (q/save-frame "resource/####.png")
+                            nil)]
+                   (assoc state :dots new-dots)
+                   state))
+  :middleware [m/fun-mode])
